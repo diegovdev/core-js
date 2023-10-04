075805ffd0a477dfbe54e52aefebeb423aef5c2a
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 08:09:20 +0700

    rename `esnext.set.symmetric-difference.v2` -> `esnext.set.symmetric-difference`

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -1418,7 +1418,7 @@ export const data = {
     // https://bugs.webkit.org/show_bug.cgi?id=267494
     safari: '18.0', // '17.0',
   },
-  'es.set.symmetric-difference.v2': {
+  'es.set.symmetric-difference': {
     bun: '1.1.1',
     // v8 ~ Chrome 122 does not properly work with set-like objects
     // https://bugs.chromium.org/p/v8/issues/detail?id=14559
@@ -2306,8 +2306,6 @@ export const data = {
   'esnext.set.some': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.set.symmetric-difference.v2': null,
-  // TODO: Remove from `core-js@4`
   'esnext.set.union.v2': null,
   'esnext.string.cooked': {
   },
@@ -2621,7 +2619,6 @@ export const renamed = new Map([
   ['esnext.map.group-by', 'es.map.group-by'],
   ['esnext.object.group-by', 'es.object.group-by'],
   ['esnext.promise.with-resolvers', 'es.promise.with-resolvers'],
-  ['esnext.set.symmetric-difference.v2', 'es.set.symmetric-difference.v2'],
   ['esnext.set.union.v2', 'es.set.union.v2'],
 ]);
 
