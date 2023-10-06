1e7eb974471e8cc988ce8fd336dd49a6c98dcf8b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-06 09:49:01 +0700

    enforce trailing commas in multiline object literals

--- tests/eslint/eslint.config.js
+++ tests/eslint/eslint.config.js
@@ -995,7 +995,7 @@ const noAsyncAwait = {
   'promise/prefer-await-to-then': OFF,
 };
 
-const useES3Syntax = {
+const useES5Syntax = {
   ...noAsyncAwait,
   // disallow logical assignment operator shorthand
   'logical-assignment-operators': [ERROR, NEVER],
@@ -1021,8 +1021,6 @@ const useES3Syntax = {
   'prefer-spread': OFF,
   // require template literals instead of string concatenation
   'prefer-template': OFF,
-  // disallow trailing commas in multiline object literals
-  '@stylistic/js/comma-dangle': [ERROR, NEVER],
   // prefer lookarounds over capturing group that do not replace
   'regexp/prefer-lookaround': [ERROR, { lookbehind: false, strictTypes: true }],
   // enforce using named capture group in regular expression
@@ -1694,7 +1692,7 @@ export default [
     languageOptions: {
       ecmaVersion: 5,
     },
-    rules: useES3Syntax,
+    rules: useES5Syntax,
   },
   {
     files: [
