283b9f2d9eaf28dab6249fe16cc003470e759470
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-21 01:05:51 +0700

    extract `DataView#{ setInt8, setUint8 }` fixes as separate modules

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -769,6 +769,7 @@ export const data = {
     'react-native': '0.74',
     safari: '16.0',
   },
+  // TODO: recheck
   'es.array-buffer.constructor': {
     chrome: '28',
     edge: '14',
@@ -776,6 +777,7 @@ export const data = {
     hermes: '0.1',
     safari: '12.0',
   },
+  // TODO: recheck
   'es.array-buffer.is-view': {
     chrome: '32',
     firefox: '29',
@@ -783,6 +785,7 @@ export const data = {
     ie: '11',
     safari: '7.1',
   },
+  // TODO: recheck
   'es.array-buffer.slice': {
     chrome: '31',
     firefox: '46',
@@ -809,6 +812,7 @@ export const data = {
     firefox: '122',
     safari: '17.4',
   },
+  // TODO: recheck
   'es.data-view.constructor': {
     chrome: '26',
     firefox: '15',
@@ -816,6 +820,24 @@ export const data = {
     ie: '10',
     rhino: '1.7.13',
     safari: '7.1',
+  },
+  // TODO: recheck
+  'es.data-view.set-int8': {
+    chrome: '26',
+    firefox: '15',
+    hermes: '0.1',
+    ie: '10',
+    rhino: '1.7.13',
+    safari: '8.0',
+  },
+  // TODO: recheck
+  'es.data-view.set-uint8': {
+    chrome: '26',
+    firefox: '15',
+    hermes: '0.1',
+    ie: '10',
+    rhino: '1.7.13',
+    safari: '8.0',
   },
   'es.date.to-json': {
     chrome: '26',
