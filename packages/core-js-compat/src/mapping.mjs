ee0d01fbac24f9b06fcb52794ead7ef4675c6ea8
Denis Pushkarev <zloirock@zloirock.ru>
2024-03-24 18:17:43 +0700

    change engine keys for compat tools / data

--- packages/core-js-compat/src/mapping.mjs
+++ packages/core-js-compat/src/mapping.mjs
@@ -180,7 +180,7 @@ export default {
   ],
   // https://github.com/mdn/browser-compat-data/blob/main/browsers/chrome_android.json
   // https://github.com/mdn/browser-compat-data/blob/main/docs/matching-browser-releases/index.md#version-numbers-for-chrome-for-android
-  ChromeToChromeAndroid(chrome) {
+  ChromeToChromeMobile(chrome) {
     return chrome <= 18 ? 18
          : chrome <= 25 ? 25
          : chrome;
@@ -189,7 +189,7 @@ export default {
   // https://github.com/mdn/browser-compat-data/blob/main/browsers/samsunginternet_android.json
   // https://en.wikipedia.org/wiki/Samsung_Internet#History
   // https://github.com/mdn/browser-compat-data/blob/main/docs/matching-browser-releases/index.md#samsung-internet
-  ChromeAndroidToSamsung: [
+  ChromeMobileToSamsung: [
     [18, '1.0'],
     [28, '1.5'],
     [34, '2.0'],
@@ -222,7 +222,7 @@ export default {
   ],
   // https://github.com/mdn/browser-compat-data/blob/main/browsers/opera_android.json
   // https://forums.opera.com/category/20/opera-for-android
-  ChromeAndroidToOperaAndroid: [
+  ChromeMobileToOperaMobile: [
     [59, 43],
     [60, 44],
     [61, 45],
@@ -270,7 +270,7 @@ export default {
   // Need to find another data source since release notes are not available for new versions.
   // https://github.com/mdn/browser-compat-data/blob/main/browsers/oculus.json
   // https://whatmyuseragent.com/browser/oc/oculus-browser
-  ChromeAndroidToQuest: [
+  ChromeMobileToQuest: [
     [57, '3.0'],
     [61, '4.0'],
     [66, '5.0'],
@@ -305,7 +305,7 @@ export default {
     [126, '34.0'],
   ],
   // https://github.com/mdn/browser-compat-data/blob/main/browsers/firefox_android.json
-  FirefoxToFirefoxAndroid(firefox) {
+  FirefoxToFirefoxMobile(firefox) {
     return firefox <= 4 ? 4 : firefox;
   },
   // https://github.com/oven-sh/bun/releases
